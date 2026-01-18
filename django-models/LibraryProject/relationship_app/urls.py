0ea8b17eedcf598cae612608302e6a51320543f6
Abdulmenan Amin <abdullahtate3@gmail.com>
2026-01-18 16:18:20 +0300

    Fixed string-match issue in urls.py in relationship_app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,9 +4,6 @@ from . import views
 
 urlpatterns = [
     path("books/", views.list_books, name="list_books"),
-    path("books/add/", views.add_book, name="add_book"),
-    path("books/<int:book_id>/edit/", views.edit_book, name="edit_book"),
-    path("books/<int:book_id>/delete/", views.delete_book, name="delete_book"),
     path("libraries/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
     path("register/", views.register, name="register"),
     path(
@@ -22,4 +19,7 @@ urlpatterns = [
     path("admin-role/", views.admin_view, name="admin_view"),
     path("librarian-role/", views.librarian_view, name="librarian_view"),
     path("member-role/", views.member_view, name="member_view"),
+    path("books/add/", views.add_book, name="add_book"),
+    path("books/<int:book_id>/edit/", views.edit_book, name="edit_book"),
+    path("books/<int:book_id>/delete/", views.delete_book, name="delete_book"),
 ]
