5094cbf7a489a4b997588248665e6b9c027ed969
Abdulmenan Amin <abdullahtate3@gmail.com>
2026-01-18 16:12:26 +0300

    Fixed import issue of views.py in relationship_app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,7 +6,8 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import redirect, render
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 from django.http import HttpResponse
 
 
