438293594fd123f49d8c7852e473befb4cd8c32b
Abdulmenan Amin <abdullahtate3@gmail.com>
2026-01-18 14:39:50 +0300

    Fixed string-matching issue

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,13 +2,15 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 
 # Query all books by a specific author
-author = Author.objects.get(name="George Orwell")
+author_name = "George Orwell"
+author = Author.objects.get(name=author_name)
 books_by_author = Book.objects.filter(author=author)
 print("Books by author:", books_by_author)
 
 
 # List all books in a library
-library = Library.objects.get(name="Central Library")
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
 library_books = library.books.all()
 print("Books in library:", library_books)
 
