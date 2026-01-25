dcb4ba32369f7841d0db414e3d9e6a278f5bf27c
Abdulmenan Amin <abdullahtate3@gmail.com>
2026-01-25 17:48:38 +0300

    Fixed implementation of a custom user model

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User, AbstractUser, BaseUserManager
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
@@ -62,50 +62,3 @@ class UserProfile(models.Model):
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance, role='Member')
-
-
-class CustomUserManager(BaseUserManager):
-    def create_user(self, username, email=None, password=None, date_of_birth=None, profile_photo=None, **extra_fields):
-        if not username:
-            raise ValueError("The Username must be set")
-
-        email = self.normalize_email(email)
-        user = self.model(
-            username=username,
-            email=email,
-            date_of_birth=date_of_birth,
-            profile_photo=profile_photo,
-            **extra_fields
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, email=None, password=None, date_of_birth=None, profile_photo=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("is_active", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-
-        return self.create_user(
-            username=username,
-            email=email,
-            password=password,
-            date_of_birth=date_of_birth,
-            profile_photo=profile_photo,
-            **extra_fields
-        )
-
-
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
-
-    objects = CustomUserManager()
-
-    def __str__(self):
-        return self.username
