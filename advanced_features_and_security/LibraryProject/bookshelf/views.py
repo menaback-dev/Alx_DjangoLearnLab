e4d6c51e4bf7c0859e672489bdf10d22f55c5184
Abdulmenan Amin <abdullahtate3@gmail.com>
2026-01-25 18:20:46 +0300

    fixed permission and group  managing task issue

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
-# Create your views here.
+
+@permission_required("bookshelf.can_view", raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return HttpResponse(", ".join([book.title for book in books]))
