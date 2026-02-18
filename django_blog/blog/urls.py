a281274a94a7c984827270a82b833cfdd31de853
Abdulmenan Amin <abdullahtate3@gmail.com>
2026-02-18 11:07:29 +0300

    change minor problem on urls configuration

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 from .views import (
     home,
+    register,
+    profile,
     PostListView,
     PostDetailView,
     PostCreateView,
@@ -25,8 +28,10 @@ urlpatterns = [
     path("post/<int:pk>/comments/new/", CommentCreateView.as_view(), name="comment-create"),
     path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="comment-update"),
     path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
-    path('register/', views.register, name='register'),
-    path('profile/', views.profile, name='profile'),
+    path('register/', register, name='register'),
+    path("login/", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path('profile/', profile, name='profile'),
     path('accounts/', include('django.contrib.auth.urls')),
     path("tags/<slug:tag_slug>/", PostByTagListView.as_view(), name="posts-by-tag"),
     path("search/", SearchResultsView.as_view(), name="search"),
